b28fe01992f15126c9820834defe86a615ce0a88
Will Russell <will@will-russell.com>
2019-06-03 02:26:05 +0100

    on_message to use one channel
    Permissions aren't required

--- foodflex/periods/messages.py
+++ foodflex/periods/messages.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import datetime
 import random
 from builtins import bot
-from ..util.data import *
 from ..util import config
 from . import submissions
 from . import voting
@@ -12,19 +11,17 @@ logger = config.initilise_logging()
 
 @bot.event    
 async def on_message(message):
-    submission_channel = bot.get_channel(config.config['submission_channel_id'])
-    voting_channel = bot.get_channel(int(config.config['voting_channel_id']))
-    dev_channel = bot.get_channel(config.config['dev_channel_id'])
+    food_flex_channel = bot.get_channel(config.config['food_flex_chat_id'])
     now = datetime.datetime.now()
     hour = int(now.strftime("%H"))
     minute = int(now.strftime("%M"))
-    second = int(now.strftime("%S"))
     await bot.process_commands(message)
 
-    if len(message.attachments) > 0 and (hour >= 12 and hour <= 23) and (message.channel == submission_channel): # SUBMISSION
-        logger.info("Submission from " + message.author.nick)
-        await submissions.process_submission(message, submission_channel)
+    if message.channel == food_flex_channel:
+        if len(message.attachments) > 0 and hour >= 12 and hour <= 23: # SUBMISSION
+            logger.info("Submission from " + message.author.nick)
+            await submissions.process_submission(message, food_flex_channel)
 
-    if len(message.attachments) == 0 and (hour >= 00 and hour < 12) and message.channel == voting_channel and (len(str(message.clean_content)) == 1 or message.clean_content == ':b:'): # VOTING
-        logger.info("Vote from: " + message.author.nick + ", Vote: " + str(message.clean_content))
-        await voting.check_vote(message, voting_channel)
+        if len(message.attachments) == 0 and (hour >= 00 and hour < 12) and (len(str(message.clean_content)) == 1 or message.clean_content == ':b:'): # VOTING
+            logger.info("Vote from: " + message.author.nick + ", Vote: " + str(message.clean_content))
+            await voting.check_vote(message, food_flex_channel)
--- foodflex/util/setup_period.py
+++ foodflex/util/setup_period.py
@@ -8,12 +8,6 @@ from . import config
 
 logger = config.initilise_logging()
 
-async def channel_permissions(before, after, channel_before, channel_after):
-    guild = bot.get_guild(config.config['server_id'])
-    await channel_before.set_permissions(guild.default_role, send_messages=before)
-    await channel_after.set_permissions(guild.default_role, send_messages=after)
-    logger.debug("Permissions updated")
-
 def reset_daily_data():
     daily_data['submissions'].clear()
     daily_data['votes'].clear()
