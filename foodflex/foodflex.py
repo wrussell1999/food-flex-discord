c2e91a2b7329f3505078acda82c5159144027aa2
Will Russell <will@will-russell.com>
2019-06-04 13:24:18 +0100

    Fixed formatting and line length

--- foodflex/foodflex.py
+++ foodflex/foodflex.py
@@ -1,3 +1,10 @@
+from .periods.leaderboard import *
+from .periods.results import *
+from .periods.voting import *
+from .periods.submissions import *
+from .periods import messages
+from .util import setup_period
+from .util import flex_commands
 import discord
 from discord.ext import commands
 import datetime
@@ -10,14 +17,6 @@ from .util.data import *
 bot = commands.Bot(command_prefix='flex:', owner_id=config.config['admin_id'])
 builtins.bot = bot
 
-from .util import flex_commands
-from .util import setup_period
-from .periods import messages
-from .periods.submissions import *
-from .periods.voting import *
-from .periods.results import *
-from .periods.leaderboard import *
-
 
 def main():
     logger = config.initilise_logging()
@@ -27,7 +26,7 @@ def main():
 
 
 @bot.event
-async def on_ready(): 
+async def on_ready():
     logger.info("Food Flex is online!")
 
 
@@ -42,21 +41,34 @@ async def check_time_periods():
 
         if hour == 13 and minute == 00:
             await submission_period(channel)
+
         elif hour == 23 and minute == 00:
             logger.info("1 hour left for submissions")
-            embed = discord.Embed(title="1 hour left for submissions", 
+            embed = discord.Embed(title="1 hour left for submissions",
                                   description="There's still time to submit today's flex!",
                                   colour=0xff0000)
             await channel.send(embed=embed)
-        elif ((hour == 00 and minute == 00) and len(daily_data['submissions']) > 1):
+
+        elif (hour == 00 and minute == 00) and \
+                len(daily_data['submissions']) > 1:
             await voting_period(channel)
-        elif hour == 11 and minute == 00 and len(daily_data['submissions']) > 1:
+
+        elif hour == 11 and minute == 00 and \
+                len(daily_data['submissions']) > 1:
             logger.info("1 hour left for voting")
-            embed = await embed_scoreboard(daily_data['submissions'], daily_data['votes'], "1 hour left for voting", "There's still time to vote! Here are the current scores")
-            embed.set_footer(text="Remember to vote for your submission to be valid!")
+            embed = discord.Embed(title="1 hour left for voting",
+                                  description="There's still time to vote! Here are the current scores")
+            embed = await get_embed(daily_data['submissions'],
+                                    daily_data['votes'])
+            embed.set_footer(
+                text="Remember to vote for your submission to be valid!")
             await private_vote_reminder()
             await channel.send(
                 embed=embed)
-        elif hour == 12 and minute == 00 and len(daily_data['submissions']) > 1 and len(daily_data['voters']) > 0:
+
+        elif hour == 12 and minute == 00 and \
+                len(daily_data['submissions']) > 1 and \
+                len(daily_data['voters']) > 0:
             await results_period(channel)
-        await asyncio.sleep(60) 
+
+        await asyncio.sleep(60)
