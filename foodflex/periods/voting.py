3005a156cddef26302c884a9f792ebbbc877491a
Will Russell <jetpowerss@gmail.com>
2019-06-05 14:40:03 +0100

    Voting now uses dictionaries over lists
    Rework on how voting validation is done. Now a lot smaller and cleaner

--- foodflex/periods/voting.py
+++ foodflex/periods/voting.py
@@ -5,7 +5,7 @@ import random
 from builtins import bot
 from ..util.data import *
 from ..util import config
-from ..util.setup_period import *
+from ..util.setup_period import daily_data, data_dict_to_json, strings, config
 
 logger = config.initilise_logging()
 
@@ -15,30 +15,29 @@ async def voting_period(channel):
     activity = discord.Activity(name="people vote on shit food",
                                 type=discord.ActivityType.watching)
     await bot.change_presence(status=discord.Status.online, activity=activity)
-    embed = discord.Embed(title="VOTING!",
+    embed = discord.Embed(title="Voting is open",
                           description="Vote for the best cooking of the day!",
                           colour=0xff0000)
     embed.set_footer(text="Respond in the chat with the appropriate letter")
-    vote_value = 'A'
-    for value in daily_data['submissions']:
-        user = bot.get_guild(config.config['server_id']).get_member(value)
-        embed.add_field(name=user.nick, value=str(vote_value), inline=False)
-        vote_value = chr(ord(vote_value) + 1)    
-    await channel.send(embed=embed)
 
-    for value in daily_data['submissions']:
-        daily_data['votes'].append(0)
-    logger.info(str(daily_data['votes']))
-    data_dict_to_json()
+    for value in daily_data:
+        embed.add_field(name=daily_data[str(value)]['nick'],
+                        value=daily_data[str(value)]['vote_index'],
+                        inline=False)
+
+    await channel.send(embed=embed)
 
 
 async def private_vote_reminder():
-    for member_id in daily_data['submissions']:
-        if member_id not in daily_data['voters']:
-            user = bot.get_guild(config.config['server_id']).get_member(
-                member_id)
+    for user in daily_data:
+        if daily_data[str(user)]['submitted'] is True and \
+                daily_data[str(user)]['voted'] is False:
+            user = bot.get_guild(
+                config.config['server_id']).get_member(
+                str(user))
             embed = discord.Embed(title="REMINDER!",
-                                  description="Remember to vote for your submission to be valid!!!")
+                                  description="Remember to vote for " +
+                                  "your submission to be valid!!!")
             embed.set_footer(text="You will be disqualified if you don't vote")
             await user.send(embed=embed)
             logger.debug("Vote reminder sent for " + str(user.nick))
@@ -46,41 +45,25 @@ async def private_vote_reminder():
 
 async def check_vote(message):
     logger.info("Vote by: " + str(message.author.nick))
+  
+    vote = message.clean_content[0]
     if message.clean_content == ':b:' or message.clean_content == "🅱️":
-        vote_index = 1
+        vote = 'B'
+
+    user_id = str(message.author.id)
+    if daily_data[user_id]['voted'] is False or \
+            daily_data[user_id]['vote_index'] is not vote:
+        
+        daily_data[user_id]['voted'] = True
+        for user in daily_data:
+            if daily_data[str(user)]['vote_index'] is vote:
+                daily_data[str(user)]['votes'] += 1
+        data_dict_to_json()
+        
+        await message.author.send(
+            "Your vote has been submitted successfully")
     else:
-        vote_index = ord(message.clean_content[0].upper()) - 65
-    logger.debug("vote_index: " + str(vote_index))  # this is an index
-    if vote_index < len(daily_data['submissions']) and vote_index >= 0:  # Checks if it's in range
-        duplicate = check_duplicate(vote_index, message)
-        if duplicate is False:
-            await is_valid(vote_index, message)
-            await message.author.send(
-                "Your vote has been submitted successfully")
-
-
-def check_duplicate(vote_index, message):
-    if check_self_vote(vote_index, message) is True or message.author.id in daily_data['voters']:
-        return True
-    else:
-        return False
-
-
-def check_self_vote(vote_index, message):
-    if message.author.id in daily_data['submissions']:
-        voter_index = daily_data['submissions'].index(message.author.id)
-        if voter_index == vote_index:
-            logger.warn("Invalid vote: same submission")
-            return True
-    else:
-        return False
-
-
-async def is_valid(vote_index, message):
-    daily_data['voters'].append(message.author.id)
-    daily_data['votes'][vote_index] += 1
-    logger.debug("Vote List after: " + str(daily_data['votes']))
-    data_dict_to_json()
+        await message.author.send("Invalid vote!")
 
 
 @bot.command()
