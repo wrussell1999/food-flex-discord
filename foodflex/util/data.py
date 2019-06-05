9073b6b45f020a2d623c5bf7b9f0f428fded8567
Will Russell <jetpowerss@gmail.com>
2019-06-06 00:05:57 +0100

    Saving method names changed
    Renamed for readability

--- foodflex/util/data.py
+++ foodflex/util/data.py
@@ -4,10 +4,10 @@ from . import config
 with open("data/quotes.json") as quote_file:
     quotes = json.load(quote_file)
 
-with open("data/scoreboard.json") as score_file:
+with open("data/leaderboard.json") as score_file:
     overall_score = json.load(score_file)
 
-with open("data/daily_data.json") as temp_file:
+with open("data/data.json") as temp_file:
     daily_data = json.load(temp_file)
 
 with open("data/strings.json") as strings_file:
@@ -16,13 +16,13 @@ with open("data/strings.json") as strings_file:
 logger = config.initilise_logging()
 
 
-def score_dict_to_json():
+def save_leaderboard():
     logger.debug("'scoreboard' dumped to scoreboard.json")
-    with open('data/scoreboard.json', 'w') as json_file:
+    with open('data/leaderboard.json', 'w') as json_file:
         json.dump(overall_score, json_file)
 
 
-def data_dict_to_json():
+def save_data():
     logger.debug("'data' dumped to daily_data.json")
-    with open('data/daily_data.json', 'w') as json_file:
+    with open('data/data.json', 'w') as json_file:
         json.dump(daily_data, json_file)
