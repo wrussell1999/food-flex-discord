4c66376e105636e67e670fb0a4a9c5cf2aaacf7f
Will Russell <jetpowerss@gmail.com>
2019-06-05 14:06:00 +0100

    Submissions creates a dict
    Now creates a dictionary for each user, rather than creating a list of IDs

--- foodflex/periods/submissions.py
+++ foodflex/periods/submissions.py
@@ -12,30 +12,39 @@ logger = config.initilise_logging()
 
 async def submission_period(channel):
     logger.info("SUBMISSIONS")
-    activity = discord.Activity(name="people submit shit food", type=discord.ActivityType.watching)
+    activity = discord.Activity(name="people submit shit food",
+                                type=discord.ActivityType.watching)
     await bot.change_presence(status=discord.Status.online, activity=activity)
-    embed = discord.Embed(title="Submissions are open", description="Submit a picture of your cooking!", colour=0xff0000)
+    embed = discord.Embed(title="Submissions are open",
+                          description="Submit a picture of your cooking!",
+                          colour=0xff0000)
     await channel.send(embed=embed)
 
 
 async def process_submission(message, channel):
-    duplicate = False
-    for value in daily_data['submissions']:
-        if (message.author.id == value):
-            duplicate = True
-    if duplicate == False:
-        daily_data['submissions'].append(message.author.id)
-        logger.info("Submission valid")
-        await channel.send(random.choice(quotes['rude']))   
-        data_dict_to_json()
-    elif (duplicate == True):
+    user_id_key = str(message.author.id)
+    if str(message.author.id) in daily_data:
         logger.info("Submission invalid")
+    else:
+        user = bot.get_server(
+            config.config['server_id']).get_member(message.author.id)
+        daily_data[user_id_key] = {
+            "nick": str(user.nick),
+            "submitted": True,
+            "voted": False,
+            "votes": 0,
+            "vote_index": chr(ord('A') + len(daily_data))
+        }
+        await channel.send(random.choice(quotes['rude']))
+        data_dict_to_json()
+        logger.info("Submission valid")
 
 
 @bot.command()
 async def submissions(ctx):
     if await bot.is_owner(ctx.author):
-        await submission_period(bot.get_channel(config.config['food_flex_channel_id']))
+        await submission_period(bot.get_channel(
+            config.config['food_flex_channel_id']))
         reset_daily_data()
         logger.info("Submissions started manually")
         await ctx.message.delete()
@@ -44,8 +53,12 @@ async def submissions(ctx):
 @bot.command()
 async def close_submissions(ctx):
     if await bot.is_owner(ctx.author):
-        activity = discord.Activity(name="people submit shit food", type=discord.ActivityType.watching)
-        await bot.change_presence(status=discord.Status.online, activity=activity)
-        embed = discord.Embed(title="Submissions are closed", description="We are currently working hard to fix some problems! Check back later!", colour=0xff0000)
+        activity = discord.Activity(name="bugs get fixed",
+                                    type=discord.ActivityType.watching)
+        await bot.change_presence(status=discord.Status.online,
+                                  activity=activity)
+        embed = discord.Embed(title="Submissions are closed",
+                              description="We are currently working hard to fix some problems! Check back later!",
+                              colour=0xff0000)
         channel = bot.get_channel(config.config['food_flex_channel_id'])
         await channel.send(embed=embed)
