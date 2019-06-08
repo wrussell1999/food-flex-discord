6ed729edb3139fd740e115f8dfe5ac5aac762755
Daniel Spencer <danielfspencer1@gmail.com>
2019-06-08 16:45:09 +0100

    Backported fix for save_data

--- foodflex/periods/submissions.py
+++ foodflex/periods/submissions.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import datetime
 import random
 from builtins import bot
-from ..util.data import daily_data, quotes
+from ..util.data import daily_data, quotes, save_data
 from ..util import config
 
 logger = config.initilise_logging()
