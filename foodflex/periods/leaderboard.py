48541a40636804fe94e4a090973dda6e0f5149db
Will Russell <jetpowerss@gmail.com>
2019-06-08 18:27:06 +0100

    Leaderboard now updates
    Works without an existing message too. Adds the ID to config.json now

--- foodflex/periods/leaderboard.py
+++ foodflex/periods/leaderboard.py
@@ -2,41 +2,47 @@ import discord
 from discord.ext import commands
 import datetime
 import random
+import json
 from builtins import bot
 from ..util.data import leaderboard_data, save_leaderboard
-from ..util import config
+from ..util.config import config, save_config, initilise_logging
 
-logger = config.initilise_logging()
-sorted_scoreboard_dict = {}
+logger = initilise_logging()
 
 
 def update_score(winner, score):
     logger.debug("Score value: " + str(score))
 
-    if winner.id in leaderboard_data['users']:
-        index = leaderboard_data['users'].index(winner.id)
-        leaderboard_data['score'][index] += score
+    if winner.id not in leaderboard_data:
+        leaderboard_data[str(winner.id)] = {
+            'nick': winner.nick,
+            'score': 1
+        }
     else:
-        leaderboard_data['users'].append(winner.id)
-        leaderboard_data['score'].append(score)
+        leaderboard_data[str(winner.id)]['score'] += 1
     save_leaderboard()
 
 
 async def update_leaderboard():
-    channel = bot.get_channel(config.config['leaderboard_channel_id'])
-    sorted_scoreboard_dict = sort_leaderboard()
-    try:
+    channel = bot.get_channel(config['leaderboard_channel_id'])
+
+    users = [(leaderboard_data[key]['nick'], leaderboard_data[key]['score'])
+             for key in leaderboard_data]
+    users.sort(key=lambda tuple: tuple[1], reverse=True)
+
+    channel = bot.get_channel(config['leaderboard_channel_id'])
+    if 'leaderboard_message_id' in config:
         message = await channel.fetch_message(
-            config.config['leaderboard_message_id'])
-        await update_scores(sorted_scoreboard_dict['users'],
-                            sorted_scoreboard_dict['scores'], message)
-    except:
-        await create_leaderboard(sorted_scoreboard_dict['users'],
-                                 sorted_scoreboard_dict['scores'])
+            config['leaderboard_message_id'])
+        embed = get_embed(users)
+        await message.edit(embed=embed)
 
-
-def sort_leaderboard():  # FIX ME
-    return
+    else:
+        embed = get_embed(users)
+        await channel.send(embed=embed)
+        message_id = channel.last_message_id
+        config['leaderboard_message_id'] = message_id
+        save_config()
 
 
 async def update_scores(users, scores, message):
@@ -44,30 +50,20 @@ async def update_scores(users, scores, message):
     await message.edit(embed=embed)
 
 
-async def create_leaderboard(users, scores):
-    embed = get_embed(users, scores)
-    channel = bot.get_channel(config.config['leaderboard_channel_id'])
-    await channel.send(embed=embed)
-
-    # Get the ID of the last message
-    message_id = channel.last_message_id()
-    logger.debug(message_id)
-    config.config['leaderboard_message_id'] = message_id
-
-
-def get_embed(users, scores):
+def get_embed(users):
     now = datetime.datetime.now()
     date_str = "Overall scores this term - " + \
         str(now.day) + "/" + str(now.month) + "/" + str(now.year)
     embed = discord.Embed(
         title="Leaderboard", description=date_str, colour=0xff0000)
-    for index, val in enumerate(users):
-        user = bot.get_guild(config.config['guild_id']).get_member(val)
-        score = "Score: " + str(scores[index])
-        embed.add_field(name=user.nick, value=score, inline=False)
+
+    for value in users:
+        score = "Score: " + str(value[1])
+        embed.add_field(name=value[0], value=score, inline=False)
     return embed
 
 
 @bot.command()
 async def refresh_scores(ctx):
+    await ctx.message.delete()
     await update_leaderboard()
