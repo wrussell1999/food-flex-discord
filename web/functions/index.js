e72ff5a74382fb2de9e1898b1c61657164354dc9
Will Russell <will@wrussell.co.uk>
2020-07-26 02:42:10 +0100

    Display latest leaderboard

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const fs = require('fs');
+const parse = require('node-html-parser').parse;
+
 const app = express();
 const firebase = require("firebase");
 require("firebase/firestore");
@@ -16,14 +19,53 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
-app.get('/leaderboard', (req, res) => {
 
-    
-    res.sendFile('views/leaderboard.html', { root: __dirname });
+app.get('/leaderboard', async (req, res) => {
+    const leaderboardIdRef = db.collection("leaderboard").doc("current-leaderboard");
+    const idDoc = await leaderboardIdRef.get();
+    const leaderboardRef = db.collection("leaderboard").doc(idDoc.data().id);
+    const leaderboard = await leaderboardRef.get();
+    const data = leaderboard.data();
+
+    fs.readFile('views/leaderboard.html', 'utf8', (err, html) => {
+        if (err) {
+            throw err;
+        }
+
+        var sort_array = [];
+        for (var key in data) {
+            sort_array.push({ key: key, score: data[key].score, nick: data[key].nick });
+        }
+        sort_array.sort(function (x, y) { return y.score - x.score });
+        
+        const root = parse(html);
+        const tbody = root.querySelector('tbody');
+        for (var i = 0; i < sort_array.length; i++) {
+            var item = data[sort_array[i].key];
+            const table = `
+            <tr>
+                <th scope="row">${i + 1}</th>
+                <td>${item.nick}</td>
+                <td>${item.score}</td>
+            </tr>
+            </tbody>`
+            tbody.appendChild(table);
+        }
+        
+        // add term date
+
+        for (var element in data) {
+            
+        }
+        res.status(200).send(root.toString());
+    });
 });
 
-app.get('/submissions', (req, res) => {
-
+app.get('/submissions', async (req, res) => {
+    const weekRef = db.collection("weekly-data").doc("current-week");
+    const weekId = await weekRef.get();
+    const currentWeekRef = db.collection("weekly-data").doc(weekId.data().id);
+    const currentWeek = await currentWeekRef.get();
     res.sendFile('views/submissions.html', { root: __dirname });
 });
 
